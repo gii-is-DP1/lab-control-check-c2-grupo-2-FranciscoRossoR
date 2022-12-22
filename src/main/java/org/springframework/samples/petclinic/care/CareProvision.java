1782ef5a93276f73a0108b114101aa7f946c5d64
FRANCISCO_DE_ASÍS <franrosora@gmail.com>
2022-12-22 11:34:53 +0100

    2

--- src/main/java/org/springframework/samples/petclinic/care/CareProvision.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareProvision.java
@@ -1,6 +1,12 @@
 package org.springframework.samples.petclinic.care;
 
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Visit;
 
 import lombok.Getter;
@@ -8,8 +14,16 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class CareProvision {   
+@Entity
+public class CareProvision extends BaseEntity {
+
+    @Pattern(regexp = "^Care rated with [0-9] stars.*$")
     String userRating;
-    Visit visit;    
+
+    @ManyToOne(optional = true)
+    Visit visit;
+    
+    @ManyToOne(optional = false)
+    @NotNull
     Care care;   
 }
